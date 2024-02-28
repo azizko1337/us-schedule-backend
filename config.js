8d5debf05b66d36355de077f53584fff2e5cfc34
azizko1337 <antoni.zalupka123@gmail.com>
2024-02-28 21:29:35 +0100

    Reapired gitignore

--- config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-    PORT: 5000,
-    FRONT_END_URL: "*", //'http://192.168.0.103:3000',
-    CACHE_TTL: 1000 * 60 * 60 * 2, //2h
-}
-
-export default config;
--- routes/notFoundRoute.js
+++
@@ -1,6 +0,0 @@
-function notFoundRoute(req, res) {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Endpoint not found. Wrong URL.');
-}
-
-export default notFoundRoute;
